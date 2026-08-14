74b7406e057d56603b3e5dcd3f2002b9901b107f
anixwindy <196703104+anixwindy@users.noreply.github.com>
2026-08-14 11:56:53 +0800

    log: update 週五 2026/08/14

--- w.bat
+++ w.bat
@@ -10,8 +10,8 @@ echo   world_log          https://segv0x41.com
 echo ==========================================================
 echo.
 echo    [1]   write     - new post (create file + open editor)
-echo    [2]   publish   - rebuild posts.json, then commit + push
-echo    [3]   rebuild   - posts.json only, nothing goes public
+echo    [2]   rebuild   - posts.json only, nothing goes public
+echo    [3]   publish   - rebuild posts.json, then commit + push
 echo    [Q]   quit
 echo.
 echo ----------------------------------------------------------
@@ -19,8 +19,8 @@ choice /c 123Q /n /m "  select [1/2/3/Q]: "
 
 REM choice returns 1..4 ; errorlevel checks MUST be descending
 if errorlevel 4 goto :end
-if errorlevel 3 goto :rebuild
-if errorlevel 2 goto :publish
+if errorlevel 3 goto :publish
+if errorlevel 2 goto :rebuild
 if errorlevel 1 goto :write
 goto :end
 
@@ -30,16 +30,16 @@ echo ---- write ----------------------------------------------
 call "%~dp0new_post.bat"
 goto :menu
 
-:publish
-echo.
-echo ---- publish --------------------------------------------
-call "%~dp0push.bat"
-goto :menu
-
 :rebuild
 echo.
 echo ---- rebuild --------------------------------------------
 call "%~dp0gen_posts.bat"
+goto :menu
+
+:publish
+echo.
+echo ---- publish --------------------------------------------
+call "%~dp0push.bat"
 goto :menu
 
 :end
